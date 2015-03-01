e3c8c3b164f1347550538fa17b1fa5c0744e9d34
Andrew Dai <theman@andrewdai.co>
2015-02-28 23:33:46 -0500

    no dupes

--- routes/users.js
+++ routes/users.js
@@ -9,16 +9,24 @@ mongoose.connect('mongodb://headgoon:alternativeheroes@ds049171.mongolab.com:491
 router.get('/', function(req, res, next) {
     User.find({}, function(err,result) {
         res.send(result);
-    })
+    });
 });
 
 router.post('/', function(req, res, next) {
     var name = req.body.name;
     var lat = req.body.lat;
     var long = req.body.long;
-    new User({ name: name, lat: lat, long: long }).save(function() {
-        res.send("saved");
-    });
+
+    User.find({ name: name }, function(err, result) {
+        if (result) {
+            result.lat = lat;
+            result.long = long;
+        } else {
+            new User({ name: name, lat: lat, long: long }).save(function() {
+                res.send("saved");
+            });
+        }
+    })
 });
 
 module.exports = router;
